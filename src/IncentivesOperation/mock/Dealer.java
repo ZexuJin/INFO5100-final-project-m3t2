b170731a0fae8b73e0ed1a4cdaf76fb7b08f1740
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-12-06 13:54:02 -0800

    Add files via upload

---
+++ src/IncentivesOperation/mock/Dealer.java
@@ -0,0 +1,14 @@
+package m3.mock;
+
+public class Dealer {
+    private  String DealerID;
+    private String name;
+    private String password;
+
+    public String getDealerID() {
+        return DealerID;
+    }
+}
+
+
+
