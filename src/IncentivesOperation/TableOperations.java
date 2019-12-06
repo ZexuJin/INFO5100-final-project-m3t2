4d7e29d61fc1f6c48df56df5201146ffd56f73e5
Zexu Jin <54969533+ZexuJin@users.noreply.github.com>
2019-12-06 10:59:14 -0800

    Update TableOperations.java

--- src/IncentivesOperation/TableOperations.java
+++ src/IncentivesOperation/TableOperations.java
@@ -3,10 +3,21 @@ package m3.model;
 import java.sql.*;
 import java.util.Date;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import m3.IncentiveManagement;
 import m3.mock.Dealer;
 import m3.model.filter.Filter;
 import m3.model.offer.Offer;
+import m3.model.Incentive;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TableOperations {
 
@@ -24,18 +35,44 @@ public class TableOperations {
     }
 
 
-    /*All the functions below need to be done with the date and the getConditions(), getOffer()*
-    some functions must be added to the incentive class.*/
-
-
     //Create a new line.
-    public void Create(Incentive I) throws SQLException{
-        CreateConnection();
-        String sql = new StringBuilder().append("if not exists (select * from sysobjects where name='").append(I.getDealerID()).append("' and xtype='U')").
-                append("create table ").append(I.getDealerID()).append(" (").append("IncentiveID VARCHAR(50) primary key,").append("startDate DATETIME,").append("endDate DATETIME,").
-                append("Title VARCHAR,").append("Disclaimer VARCHAR,").append("DealerID VARCHAR,").append("FilterList VARCHAR,").append("Offer VARCHAR )").append("INSERT INTO").append(I.getDealerID()).append("VALUES (").
-                append(I.getIcentiveID()).append(",").append(I.getStartDate()).append(",").append(I.getEndDate()).append(",").append(I.getTitle()).append(",").append(I.getDisclaimer()).append(",").append(I.getDealerID()).
-                append(",").append(I.getConditions()).append(",").append(I.getOffer()).append(")").toString();
+    /*
+
+    if not exists (select * from sysobjects where name='[DealerID]' and xtype='U')
+    create table [I.IncentiveID]] (
+        IncentiveID VARCHAR(50) primary key,
+        startDate DATETIME,
+        endDate DATETIME,
+        Title VARCHAR,
+        Disclaimer VARCHAR,
+        DealerID VARCHAR,
+        FilterList(Convert to String) VARCHAR,
+        Offer(Convert to String) VARCHAR
+        )
+
+        INSERT INTO [DealerID] VALUES (
+            [I.getIncentiveID],[I.getStartDate],[I.getEndDate],
+            [I.getTitle],[I.getDisclaimer],[DealerID],
+            [I.getFilterList(Convert to String)],
+            [I.getOffer(Convert to String)]
+        )
+
+     */
+    public void Create(Incentive I) throws SQLException, JsonProcessingException {
+        CreateConnection(); //get connection;
+
+        String DealerID = I.getDealer().getDealerID(); //get DealerID
+        ObjectMapper mapper = new ObjectMapper();
+        String filterList = mapper.writeValueAsString(I.getConditions()); //convert filter list to string
+        String offer = mapper.writeValueAsString(I.getOffer()); //convert offer list to string
+
+
+        String sql = new StringBuilder().append("if not exists (select * from sysobjects where name='").append(DealerID).append("' and xtype='U')").
+                append("create table ").append(DealerID).append(" (").append("IncentiveID VARCHAR(50) primary key,").append("startDate DATETIME,").append("endDate DATETIME,").
+                append("Title VARCHAR,").append("Disclaimer VARCHAR,").append("DealerID VARCHAR,").append("FilterList VARCHAR,").append("Offer VARCHAR )").append("INSERT INTO").append(DealerID).append("VALUES (").
+                append(I.getIncentiveID()).append(",").append(I.getStartDate()).append(",").append(I.getEndDate()).append(",").append(I.getTitle()).append(",").append(I.getDisclaimer()).append(",").append(DealerID).
+                append(",").append(filterList).append(",").append(offer).append(")").toString();
+
         Statement statement = connection.createStatement();
         statement.executeUpdate(sql);
         connection.close();
@@ -45,9 +82,10 @@ public class TableOperations {
     public static void EditItem(Incentive I) throws SQLException{
         //TODO
         CreateConnection();
-        String sql = new StringBuilder().append("UPDATE IncentiveManagementDB.").append(I.getDealerID()).append().append("SET startDate='"+I.getStartDate()+"', SET endDate='"+
+        String DealerID = I.getDealer().getDealerID();
+        String sql = new StringBuilder().append("UPDATE IncentiveManagementDB.").append(DealerID).append("SET startDate='"+I.getStartDate()+"', SET endDate='"+
                 I.getEndDate()+"', SET Title='"+I.getTitle()+"', SET Disclaimer='"+I.getDisclaimer()+"', SET DealerID='"+I.getDealerID()+"', SET offer='"+
-                offer+"', SET condition='"+condition).append("WHERE IncentiveID="+IncentiveID).toString();
+                I.getConditions()+"', SET condition='"+condition).append("WHERE IncentiveID="+IncentiveID).toString();
         Statement statement = connection.createStatement();
         statement.executeUpdate(sql);
         connection.close();
@@ -63,9 +101,33 @@ public class TableOperations {
         connection.close();
     }
 
-    public List<Incentive> getIncentiveByDealer(Dealer d) throws Exception {
-        //TODO
-        // search in dealer_id's column, matching dealer_id, then add this incentive to return list
+    // Get list of incentives by DealerID
+    public List<Incentive> getIncentiveByDealer(String DealerId) {
+        List<Incentive> incentives = new ArrayList<>();
+        conn = DBConnection.CreateConnection();
+        String sql = "SELECT * FROM " + DealerId;
+        try {
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            while(!rs.next()) {
+                Incentive i = new Incentive();
+                i.setStartDate(rs.getDate("startDate"));
+                i.setEndDate(rs.getDate("endDate"));
+                i.setTitle(rs.getString("Title"));
+                i.setDisclaimer(rs.getString("Disclaimer"));
+                ObjectMapper mapper = new ObjectMapper();
+                Dealer d = mapper.readValue(rs.getString("dealer"), Dealer.class);
+                i.setDealer(d); // Json -> Dealer类
+                List<Filter> filters = mapper.readValue(rs.getString("FilterList"), new TypeReference<List<Filter>>(){});
+                i.setConditions(filters); // JSON -> List<Filter>
+                Offer offer = mapper.readValue(rs.getString("Offer"), Offer.class);
+                i.setOffer(offer); // Json -> Offer
+                incentives.add(i);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return incentives;
     }
 
 }
